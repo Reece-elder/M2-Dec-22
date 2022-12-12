9e10a560d927e6d903aa8fef7ef5c5bea37cafad
reece-elder <reece.elder@qa.com>
2022-12-12 10:55:36 +0000

    added labs

---
+++ code/001-Intro/demo.py
@@ -0,0 +1,9 @@
+# Adding a comment with #
+# Some code / text that is there for documentation, the interpreter (Python) will not read this
+# print("Hello!")
+
+# ls - List stuff, lists the directory -a to show hidden files
+# cd - change directory cd .. back a folder, cd <folder name> to go to that folder
+# python <name of file> - runs the file
+
+print("Hello World")
