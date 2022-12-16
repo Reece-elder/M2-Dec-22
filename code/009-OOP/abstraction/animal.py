c114a1af6b5a17711fd3fc658398516b120e4dcd
reece-elder <reece.elder@qa.com>
2022-12-16 15:21:11 +0000

    abstraction

---
+++ code/009-OOP/abstraction/animal.py
@@ -0,0 +1,25 @@
+
+# Abstract class it must inherit Abstract functionality
+# Importing ABC and abstractmethod from abc (abstract base class) inbuilt library
+from abc import ABC, abstractmethod
+
+class animal(ABC):
+    # because this is an abstract class, we can't create objects from it.. no need for a constructor
+    size = 0
+    name = ""
+    # Abstract method for rest
+    @abstractmethod  # Descriptor telling Python this function is abstract
+    def rest(self, hours):
+        pass # Every child MUST INCLUDE THE FUNCTION rest(self, hours) pass = does nothing 
+
+class cat(animal):
+
+    def __init__(self, fur_colour, fav_food):
+        self.fur_colour = fur_colour
+        self.fav_food = fav_food
+
+    # Overriding our abstract method
+    def rest(self, hours):
+        return f"Sleeps for {hours}"
+
+zaph = cat("Browny, blacky, beigey", "chicken sticks")
