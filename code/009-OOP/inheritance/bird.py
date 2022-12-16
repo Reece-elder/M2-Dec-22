327d093230376b92e238df19f3b2375e14672c1d
reece-elder <reece.elder@qa.com>
2022-12-16 14:09:27 +0000

    inheritance

---
+++ code/009-OOP/inheritance/bird.py
@@ -0,0 +1,9 @@
+# Bird is our parent class
+class bird:
+
+    def __init__(self, feathers, wingspan):
+        self.feathers = feathers
+        self.wingspan = wingspan
+
+    def eat(self):
+        return "*eating sounds*"
